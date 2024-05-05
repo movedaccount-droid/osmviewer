8c6f09e87f88bdaa969c05913ef5fba163495cad
git user <BACK@WORK.COM>
2024-05-05 03:49:59 +0100

    basic map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.preference)
     implementation(libs.osmdroid.android)
 }
--- app/src/main/java/ac/uk/hope/osmviewer/FirstFragment.java
+++ app/src/main/java/ac/uk/hope/osmviewer/FirstFragment.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,15 +20,23 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.preference.PreferenceManager;
 
 import ac.uk.hope.osmviewer.databinding.FragmentFirstBinding;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
-    private Context context;
-
+    private static final String TAG = FirstFragment.class.getSimpleName();
     private ActivityResultLauncher<String[]> locationPermissionLauncher;
+    private ActivityResultLauncher<String[]> networkPermissionLauncher;
+    private ActivityResultLauncher<String> storagePermissionLauncher;
+    private MapView map = null;
+
 
     @Override
     public View onCreateView(
@@ -35,6 +44,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
+        // register permissions callbacks
         locationPermissionLauncher = registerForActivityResult(
             new ActivityResultContracts.RequestMultiplePermissions(),
             result -> {
@@ -43,15 +53,46 @@ public class FirstFragment extends Fragment {
                 Boolean coarseLocationGranted = result.getOrDefault(
                         Manifest.permission.ACCESS_COARSE_LOCATION,false);
                 if (fineLocationGranted != null && fineLocationGranted) {
-                    // Precise location access granted.
+                    Log.d(TAG, "fine location granted");
                 } else if (coarseLocationGranted != null && coarseLocationGranted) {
-                    // Only approximate location access granted.
+                    Log.d(TAG, "coarse location granted");
                 } else {
-                    // No location access granted.
+                    Log.d(TAG, "no location granted");
                 }
             }
         );
 
+        networkPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestMultiplePermissions(),
+            result -> {
+                Boolean networkGranted = result.getOrDefault(
+                        Manifest.permission.ACCESS_NETWORK_STATE, false);
+                Boolean internetGranted = result.getOrDefault(
+                        Manifest.permission.INTERNET,false);
+                if (networkGranted != null && networkGranted
+                    && internetGranted != null && internetGranted) {
+                    Log.d(TAG, "internet granted");
+                } else {
+                    Log.d(TAG, "no internet granted");
+                }
+            }
+        );
+
+        storagePermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            allowed -> {
+                if (allowed) {
+                    Log.d(TAG, "storage granted");
+                } else {
+                    Log.d(TAG, "no storage granted");
+                }
+            }
+        );
+
+        // load settings from sharedpreferences
+        // TODO: should this be named as in 7 creating?
+        Configuration.getInstance().load(requireActivity(),
+                PreferenceManager.getDefaultSharedPreferences(requireActivity()));
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -61,18 +102,41 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        context = view.getContext();
-
         binding.buttonFirst.setOnClickListener(v ->
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment)
         );
 
         // setup permissions for the map
+        // TODO: follow best guidance in https://developer.android.com/training/permissions/requesting#java
         locationPermissionLauncher.launch(new String[] {
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION
         });
+
+        networkPermissionLauncher.launch(new String[] {
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET
+        });
+
+        storagePermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        // show map
+        map = (MapView) view.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
     }
 
     @Override
@@ -80,9 +144,4 @@ public class FirstFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-    private void initializeMap() {
-
-    }
-
 }
